66257fda20469d6b88665a43e5999df3ffbdf2c3
pepesan <pepesan@gmail.com>
2018-01-26 16:56:34 +0100

    mejoras en collapse

---
+++ ejercicios/hotel.js
@@ -0,0 +1,41 @@
+/*
+Buenas tardes Sr. Vaquero. 
+
+Tras hablar con mi compañero Alejandro, ya que yo que fui la persona que le llamé no me encontraba en el hotel le informo de los siguiente sobre su petición.
+
+Travelodge es una cadena inglesa y por ello muy estricta con la normativa antitabaco desde hace muchos años. Travelodge no tiene en ninguno de sus hoteles de Reino Unido, Irlanda y España habitaciones para fumadores, es más, en los hoteles de Reino Unido y amparados por su legislación cargan una multa de 150 libras a la tarjeta de cliente por el mero hecho de que haya habido indicios de que se haya fumado en la habitación con cualquier tipo de cigarro o artilugio. Debido a esto, no entiendo que desde España se tenga la más mínima benevolencia con este tema.
+
+Dicho esto, y debido a que es un cliente habitual, siempre que haga una reserva, si nos avisa con antelación (como mínimo un día antes) solicitaremos al departamento de pisos que deje la habitación que se le asigne a usted con la ventana sin seguro de apertura para que pueda abrirla por completo pero siempre con la finalidad de que usted quiere tener una mayor amplitud en la apertura de su ventana y no para infringir la normativa antitabaco de la cadena.
+
+Si tiene alguna consulta, no dude en contactar conmigo. 
+Francisco Llobregat 
+
+Asistente de dirección 
+
+
+
+Estimado Francisco:
+
+Ante todo muchas gracias por atender la petición que realicé a través de su compañero Alejandro y poder dar una alternativa a mi caso como cliente habitual. 
+
+En España se toma muy en serio el tema de las adicciones incluido la del tabaco. Por eso está prohibido su consumo en multitud de lugares incluidas las cafeterías de los hoteles. Quizá debido a la similitud de uso de los vapeadores, esos artilugios, como usted dice, pueden ser confundidos por una persona no entendida en el tema. Según la normativa se denominan: dispositivos susceptibles de liberación de nicotina.  
+
+Le informo que este tipo de dispositivos, en España no está prohibido de serie su uso en Hoteles y Cafeterías, salvo en País Vasco que tiene una equiparación al tabaco. Debido al cambio de la normativa que se produjo en 2014 en la disposición final duodécima se describen los lugares donde está prohibido su consumo. Le adjunto el enlace a la ley por si desea leerla usted mismo. 
+https://www.boe.es/boe/dias/2014/03/28/pdfs/BOE-A-2014-3329.pdf
+
+Como hotel, tiene un conjunto de términos y condiciones que incluye:
+8. Smoking is not permitted in any Travelodge hotel other than in designated smoking areas outside the hotel.  You must not smoke in any of our hotels, either in the hotel rooms or public areas, or interfere with our fire detection system or with any emergency equipment.  If you do so we may terminate your booking and reserve our rights to take any further action.  We may either (at our sole discretion) request the immediate re-payment of our reasonable costs during your stay, charge the payment card details provided at the time of booking or instruct a third party to contact you after your stay to recover our reasonable costs.  The reasonable costs we incur if you smoke in our hotel are likely to include costs for specialist cleaning, repair or replacement of damage by you to our property, the cost of the room for any period it is unusable and our administration expenses.  If you request it we will send a breakdown of these costs to the address used for the booking
+
+En este caso se menciona al acto de fumar, pero no al acto de "vapear" (vaping en inglés)que es la palabra usada para la utilización de dispositivos susceptibles de liberación de nicotina. Por lo que se sobre entiende que se permite vapear en el hotel. Salvo por la cuestión de interferir voluntariamente con los sistemas de detección de incendios, por lo que les sugería la solución de poder abrir completamente la ventana, para evitar precisamente dicha interferencia. 
+
+Respecto a la cláusula 10:
+10. You must not damage or interfere with any items belonging to us. If you do so we may terminate your booking.  You must bring any damage to our hotel or property to our immediate attention. We may either (at our sole discretion) request the immediate re-payment of our reasonable costs during your stay, charge the payment card details provided at the time of booking or instruct a third party to contact you after your stay to recover the reasonable costs for any repair, replacement or specialist cleaning we incur if you damage our hotel or property.  If you request it we will send a breakdown of these costs to the address used for the booking.
+
+Aunque podría decirse que estaría interfiriendo con un elemento propiedad del hotel, está claro que no se provoca ningún daño real a ningún bien propiedad del hotel. Aunque sí podría "perjudicar" a los trabajadores del hotel, pero cómo éstos no pertenecen al hotel ésta claúsula no se aplicaría. 
+
+Por lo que considero que ha tomado la mejor decisión posible ante este caso. 
+Una vez más agradecer esta deferencia que han tenido conmigo y espero que tengan con el resto de "vapeadores" que visiten los hoteles de su cadena.
+
+Atentamente 
+
+*/
